e7807d432f919a444bb95aafb5fdf3ae8d7cb50f
Catrina Friday <cat.friday@gmail.com>
2020-12-21 18:42:59 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+
 
 function App() {
+
+
+
   return (
     <div className="App">
       Hello World!
+      <NavBar />
     </div>
   );
 }
