d4173eab140a248bb17f9fd046d2469182ebfd2c
Catrina Friday <cat.friday@gmail.com>
2020-12-24 16:55:02 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavBar from './NavBar';
 
 
@@ -9,11 +10,15 @@ function App() {
 
 
   return (
+    <BrowserRouter>
+    
     <div className="App">
       Hello World!
       True Tax Prep
       <NavBar />
     </div>
+    
+    </BrowserRouter>
   );
 }
 
